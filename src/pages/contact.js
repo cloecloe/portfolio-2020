849c9333eb41f0e1b38a3d83e899ca3886fab0fa
Cloe Furlan <cloe.furlan@yahoo.fr>
2020-10-09 13:25:40 -0400

    Responsiveness of contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,8 +9,8 @@ export default function Contact() {
       <h1 className="text-center flex flex-wrap">
         Interested in a collaboration or learning more?
       </h1>
-      <h2 className="my-8">Reach out!</h2>
-      <a href="mailto:cloe.furlan@yahoo.fr">cloe.furlan@yahoo.fr</a>
+      <h2 className="my-6">Reach out!</h2>
+      <a href="mailto:cloe.furlan@yahoo.fr" className="my-3">cloe.furlan@yahoo.fr</a>
       <div className="icons">
         <a href="https://www.linkedin.com/in/cloe-f/">
           <i class="fab fa-linkedin"></i>
