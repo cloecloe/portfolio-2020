6a420cb6b6e8df5c6bf0a7f484b8e1eba232395a
Cloe Furlan <cloe.furlan@yahoo.fr>
2020-10-08 22:30:24 -0400

    Responsiveness of the background header

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,28 +1,43 @@
 import React from 'react'
 import { Link } from "gatsby"
 
-export default function Navbar() {
-  return (
-    <section id="navbar" className="fixed bg-white opacity-75 w-full z-10">
-      <div className="flex justify-between items-center py-2 container">
-        <Link className="pr-6 my-1" to="#header">
-          <img src="../images/logocloe.png" alt="logo Cloe"/>
-        </Link>
-        <div className="right-content flex">
-          <Link className="pr-6" to="#about">
-            About
+class Navbar extends React.Component {
+  //   state = {
+  //   navOpacity: 1
+  // };
+
+  // componentDidMount() {
+  //   document.addEventListener("scroll", () => {
+  //     const opacity = window.scrollY < 600 ? 1 : 0.8;
+
+  //     this.setState({ navOpacity: opacity });
+  //   });
+  // }
+  render() {
+    return (
+      <section id="navbar" className="fixed bg-white w-full z-10 opacity-75">
+        <div className="flex justify-between items-center py-2 container">
+          <Link className="pr-6 my-1" to="#header">
+            <img src="../images/logocloe.png" alt="logo Cloe"/>
           </Link>
-          <Link className="pr-6" to="#technologies">
-            Technologies
-          </Link>
-          <Link className="pr-6" to="#projects">
-            Projects
-          </Link>
-          <Link className="pr-6" to="#contact">
-            Contact
-          </Link>
+          <div className="right-content flex">
+            <Link className="pr-6" to="#about">
+              About
+            </Link>
+            <Link className="pr-6" to="#technologies">
+              Technologies
+            </Link>
+            <Link className="pr-6" to="#projects">
+              Projects
+            </Link>
+            <Link className="pr-6" to="#contact">
+              Contact
+            </Link>
+          </div>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  }
 }
+
+export default Navbar;
