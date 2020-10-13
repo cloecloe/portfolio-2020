15a247b438c2b7090a2d3c7b5b0187745e5e8d38
Cloe Furlan <cloe.furlan@yahoo.fr>
2020-10-13 14:43:03 -0400

    Navbar responsiveness with scrolling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,11 +16,11 @@ export default function Navbar() {
 
   return (
     <section id="navbar" className="fixed bg-white w-full z-10 opacity-75">
-      <div className="container flex justify-center md:justify-between items-center py-2">
-        <button className="my-1 hidden md:flex md:items-center" onClick={() => scrollTo('#header')}>
+      <div className="container flex justify-center sm:justify-between items-center py-2">
+        <button className="my-1 hidden sm:flex sm:items-center" onClick={() => scrollTo('#header')}>
           <img src="../images/logocloe.png" alt="logo Cloe"/>
         </button>
-        <div className="right-content flex">
+        <div className="right-content hidden sm:flex">
           <button className="px-4 py-2 flex items-center" onClick={() => scrollTo('#about')}>
             About
           </button>
@@ -30,12 +30,23 @@ export default function Navbar() {
           <button className="px-4 py-2 flex items-center" onClick={() => scrollTo('#projects')}>
             Projects
           </button>
-          <button className="px-4 py-2 hidden md:flex md:items-center" onClick={() => scrollTo('#contact')}>
+          <button className="px-4 py-2 flex items-center" onClick={() => scrollTo('#contact')}>
             Contact
           </button>
-          <button className="ml-1 block md:hidden contact" onClick={() => scrollTo('#contact')}>
+        </div>
+        <div className="right-content flex sm:hidden">
+          <a className="px-4 py-2 flex items-center" href="#about">
+            About
+          </a>
+          <a className="px-4 py-2 flex items-center" href="#technologies">
+            Technologies
+          </a>
+          <a className="px-4 py-2 flex items-center" href="#projects">
+            Projects
+          </a>
+          <a className="ml-1 contact" href="#contact">
             <i class="far fa-envelope"></i>
-          </button>
+          </a>
         </div>
       </div>
     </section>
