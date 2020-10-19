6591ab87e9af9ae046e9a8a6a683984820216b3d
Cloe Furlan <cloe.furlan@yahoo.fr>
2020-10-19 13:08:40 -0400

    Changed header button

--- src/pages/header.js
+++ src/pages/header.js
@@ -13,7 +13,7 @@ export default function Header() {
           <h1 className="mb-10 mt-2 md:max-w-2xl text-center md:text-left">
             From recruiter, to web developer
           </h1>
-          <Button textContent="Discover my story" link="#about" />
+          <Button textContent="Discover my profile" link="#about" />
         </div>
         {/* <div className="geometric-elements flex">
           <img
